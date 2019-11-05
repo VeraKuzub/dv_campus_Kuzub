7a73cd2b7f0a8189c520b63238f2f10ca89e647a
Vera Kuzub <v.kuzub@gmail.com>
2019-11-05 09:38:10 +0200

    configured Grunt for Verak/luma theme, and added outputSourceFiles: true in file less.js

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,32 @@
+module.exports = {
+    verak_luma_uk_UA: {
+        area: 'frontend',
+        name: 'Verak/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    verak_luma_en_US: {
+        area: 'frontend',
+        name: 'Verak/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    verak_luma_ru_RU: {
+        area: 'frontend',
+        name: 'Verak/luma',
+        locale: 'ru_RU',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
