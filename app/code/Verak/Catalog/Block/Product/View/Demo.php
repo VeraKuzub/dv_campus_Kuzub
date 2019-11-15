ed52f0ca344ee078839455991f642e3fef24b467
Vera Kuzub <v.kuzub@gmail.com>
2019-11-15 13:59:16 +0200

    created my own block Demo.php, rewrote template demo.phtml, used magic method '_call'

---
+++ app/code/Verak/Catalog/Block/Product/View/Demo.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Verak\Catalog\Block\Product\View;
+
+/**
+ * Class Demo
+ * 
+ * @package Verak\Catalog\Block\Product\View
+ * 
+ * @method int getNumberVariable()
+ * @method string getStringVariable()
+ */
+class Demo extends \Magento\Framework\View\Element\Template
+{
+
+}
