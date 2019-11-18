24fd392264c39c49b2c922cb3f148b9884375d0a
Vera Kuzub <v.kuzub@gmail.com>
2019-11-18 11:00:57 +0200

    added block Demo.php with template demo.phtml in theme Verak/luma to the Product Page after block with options, but before the tabs

--- app/code/Verak/Catalog/Block/Product/View/Demo.php
+++ app/code/Verak/Catalog/Block/Product/View/Demo.php
@@ -7,8 +7,7 @@ namespace Verak\Catalog\Block\Product\View;
  * Class Demo
  * 
  * @package Verak\Catalog\Block\Product\View
- * 
- * @method int getNumberVariable()
+ *
  * @method string getStringVariable()
  */
 class Demo extends \Magento\Framework\View\Element\Template
